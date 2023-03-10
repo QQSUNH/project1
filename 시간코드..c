eb1339038c6d7149d89114f49d537319a03e51a7
imnahyun <dlaskgus147@naver.com>
2023-03-10 12:41:30 +0900

    Create 시간코드..c

---
+++ 시간코드..c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<time.h>
+int main()
+{
+	int time2 = time(NULL); //시작 시점
+	printf("%d\n", time2);
+	getchar();
+	int time3 = time(NULL); //끝나는 시점
+	printf("%d\n", time3);
+	printf("%d\n", time3-time2); //소요시간 
+
+}
+
+
+
+if (TRYCOUNT == 5) {
+	int hint = rand()%10
+	while (hint == computer[0] || hint == computer[1] || hint == computer[2]) {
+		hint = rand() % 10;
+
+	}
+	printf("힌트: %d는 정답이 아닙니다 \n", hint);
+		
+
+
+}
